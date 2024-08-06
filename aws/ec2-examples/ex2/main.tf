d129d199844ba73283d86cc80b6e7a1723d0fd69
vutaplam <150327267+vutaplam@users.noreply.github.com>
2024-08-06 17:54:53 +0530

    Create main.tf

---
+++ aws/ec2-examples/ex2/main.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = "vpc-0bdd558ecf8140f50"
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_instance" "web-server" {
+  ami = "ami-04a81a99f5ec58529"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.allow_tls.name]
+  user_data = file("./web.sh")
+}
